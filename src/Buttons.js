81347bcbccd458d77f7c2f6fcb8bee7c022d7b7c
EstiTw <estitwitowork@gmail.com>
2022-11-07 15:41:45 +0200

    showing message when data is an empty array

--- src/Buttons.js
+++ src/Buttons.js
@@ -2,8 +2,10 @@ import React from "react";
 import { useGlobalContext } from "./context";
 
 const Buttons = () => {
-  const { isLoading, page, nbPages, handlePage } = useGlobalContext();
+  const { isLoading, page, nbPages, handlePage, hits } = useGlobalContext();
 
+  if (isLoading === false && hits.length === 0)
+    return <h5>No matching articles were found for this value</h5>;
   return (
     <div className="btn-container">
       <button disabled={isLoading} onClick={() => handlePage("dec")}>
--- src/context.js
+++ src/context.js
@@ -28,7 +28,6 @@ const AppProvider = ({ children }) => {
     try {
       const response = await fetch(url);
       const data = await response.json();
-      //TODO: handle - data is empty array, nbPages == 0
       dispatch({
         type: SET_STORIES,
         payload: { hits: data.hits, nbPages: data.nbPages },
